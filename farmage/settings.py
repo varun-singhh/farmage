9a75942d11055ac02037ab4dbf55aaeb406856ab
varun-singhh <47475007+varun-singhh@users.noreply.github.com>
2020-08-21 01:31:40 +0530

    Update settings.py

--- farmage/settings.py
+++ farmage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@pw@ycp--3u2#cx&2s0e6k32!+5_r9l!d5!+kg59jtwfnj3d&y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['farmage.herokuapp.com']
+ALLOWED_HOSTS = ['farmage.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
