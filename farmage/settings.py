7300969557ba290be7b732005c844bc85344c143
varun-singhh <47475007+varun-singhh@users.noreply.github.com>
2020-08-21 15:35:34 +0530

    Update settings.py

--- farmage/settings.py
+++ farmage/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'farmage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-]
+
 
